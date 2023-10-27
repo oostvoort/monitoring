95e667361c2315e85fb7983a60c5dc1ce3d50ee0
miko-ov <miko@oostvoort.com>
2023-10-27 11:00:59 +0800

    Remove prefix in release

--- release.config.js
+++ release.config.js
@@ -1,10 +1,13 @@
 // release.config.js
 module.exports = {
-    branches: ["main"],
+    branches: ['main'],
     plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/github"
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      ['@semantic-release/github', {
+        assets: 'charts/*.tgz',
+        tarballUrl: 'https://github.com/:oostvoort/:monitoring/archive/${nextRelease.gitTag}.tar.gz'  // Override the tarballUrl template
+      }]
     ],
   };
   
