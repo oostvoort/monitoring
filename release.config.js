a4ce785de4f0685879c9a5a82bd12033b969df18
miko-ov <miko@oostvoort.com>
2023-10-27 10:56:05 +0800

    add the required package.json for semantic versioning

---
+++ release.config.js
@@ -0,0 +1,10 @@
+// release.config.js
+module.exports = {
+    branches: ["main"],
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/github"
+    ],
+  };
+  
